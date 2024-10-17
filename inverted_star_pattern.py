b93b8014df84648a6d4f182d40e1cbe75798671e
Deepak .R <deepakravikumar04@gmail.com>
2024-10-17 23:38:56 +0530

    Create inverted_star_pattern.py

---
+++ inverted_star_pattern.py
@@ -0,0 +1,7 @@
+def inverted_star_pattern_recursive(height):
+	if height > 0:
+		print("*" * height)
+		inverted_star_pattern_recursive(height - 1)
+
+height = 5
+inverted_star_pattern_recursive(height)
